a6d36f6d3aa6f46f6b3bbea135e103b8737ed110
omarrrefaatt <119802537+omarrrefaatt@users.noreply.github.com>
2023-12-10 15:05:53 +0200

    spectrogramdone

--- main.py
+++ main.py
@@ -939,10 +939,11 @@ class Ui_MainWindow(object):
         hl = 512 # number of samples per time-step in spectrogram
         hi = 128 # Height of image
         wi = 384 # Width of image
+        max_frequency = np.max(self.frequencies)
         window = y[0:wi*hl]
-        S = librosa.feature.melspectrogram(y=window, sr=sr, n_mels=hi, fmax=8000,hop_length=hl)
+        S = librosa.feature.melspectrogram(y=window, sr=sr, n_mels=hi, fmax = max_frequency,hop_length=hl)
         S_dB = librosa.power_to_db(S, ref=np.max)
-        img = librosa.display.specshow(S_dB, x_axis='time', y_axis='mel', sr=sr, fmax=8000, ax=ax)
+        img = librosa.display.specshow(S_dB, x_axis='time', y_axis='mel', sr=sr, fmax = max_frequency, ax=ax)
         canvas.draw()
 
     def convertToWavFile(self,data,fs):
